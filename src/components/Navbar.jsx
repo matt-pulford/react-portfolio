456e5edf79ef3818a1d7d0b15d73ffd61445e505
Matt Pulford <mattyp666@outlook.com>
2023-11-27 15:17:44 -0500

    Simplify Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,24 @@
 // Navbar.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
+ 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  useEffect(() => {
-    const updateMousePosition = (e) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-
-    document.addEventListener('mousemove', updateMousePosition);
-
-    return () => {
-      document.removeEventListener('mousemove', updateMousePosition);
-    };
-  }, []);
-
   return (
-    <div className="navbar" style={{ position: 'relative' }}>
-      <a href="index.html" class="brand"><div>Your Brand</div></a>
+    <div className="navbar">
+
+        <a href="index.html" className="brand" style={{ height: '100%' }}>
+          <div className='name'>Matt Pulford</div>
+        </a>
+
+      
+
+
       <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
         <div className="link">Recent Projects</div>
         <div className="link">Services</div>
@@ -33,14 +27,8 @@ const Navbar = () => {
       <div className="hamburger" onClick={toggleNav}>
         &#9776;
       </div>
-      <div
-        className="pointer-circle"
-        style={{
-          position: 'absolute',
-          top: mousePosition.y,
-          left: mousePosition.x,
-        }}
-      ></div>
+ 
+
     </div>
   );
 };
