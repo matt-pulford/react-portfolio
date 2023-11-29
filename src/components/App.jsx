17223fb704d7254e818ffe23fd7d17fb75e9fc60
Matt Pulford <mattyp666@outlook.com>
2023-11-29 17:20:06 -0500

    Add Landing Page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,25 @@ import Home from './Home';
 import RecentProjects from './RecentProjects';
 import Contact from './Contact';
 import Services from './Services';
+import backgroundImage from '../business-2846221-grey.webp';
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          {/* Set the Home route to be the default route without a visible link in the Navbar */}
-          <Route path="/" element={<Home />} />
-          <Route path="/recent-projects" element={<RecentProjects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
+      <div className="background-container">
+        <img src={backgroundImage} alt="Background" className="background-image" />
+      </div>
+      <div className="content-container">
+        <div className="App">
+          <Navbar />
+          <Routes>
+            {/* Set the Home route to be the default route without a visible link in the Navbar */}
+            <Route path="/" element={<Home />} />
+            <Route path="/recent-projects" element={<RecentProjects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
