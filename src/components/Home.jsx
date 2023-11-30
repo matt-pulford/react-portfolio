e881d5fa175cb08a1982365ed2f600bf87caddec
Matt Pulford <mattyp666@outlook.com>
2023-11-30 16:41:01 -0500

    Update Home Layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,17 @@ import './landing-area.css';
 
 const Home = () => {
   return (
-    <div className='landing-container'>
-      <div className='landing-content'>
-        <div className='text-section'>
-          <h1 className='tagline'>Hello. I'm Matthew Pulford, a Full Stack Developer. </h1>
-          <p className='description'>I'm dedicated to continuous learning, seeking elegant solutions to real-world problems in the ever-evolving landscape of web development.</p>
-        </div>
-        <div className='buttons-container'>
-          <button>Portfolio</button>
-          <button className='contact-button'>Contact</button>
+    <div className='home'>
+      <div className='landing-container'>
+        <div className='landing-content'>
+          <div className='text-container'>
+            <h1 className='tagline'>Hello. I'm Matt Pulford, a Full Stack Developer. </h1>
+            <p className='description'>I'm dedicated to continuous learning, seeking elegant solutions to real-world problems in the ever-evolving landscape of web development.</p>
+          </div>
+          <div className='buttons-container'>
+            <button>Portfolio</button>
+            <button className='contact-button'>Contact</button>
+          </div>
         </div>
       </div>
     </div>
