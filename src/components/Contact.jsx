4dfa018609fa5ecd6bec0ee4ce9dbc5eb89f2405
Matt Pulford <mattyp666@outlook.com>
2023-12-06 15:39:17 -0500

    Start Contact Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,46 @@
-// Home.js
 import React from 'react';
 import './contact.css'
 
 const Contact = () => {
+  const handleSubmit = () => {
+    // You can add your logic here for handling the form submission
+    console.log('Form submitted!');
+  };
+
   return (
-    <div>
-      <div className='contact-container'>
-        <div className='contact-form'>
-        <div className='contact-text'>
-
+    <div className='contact'>
+      <div className='contact-area'>
+        <div className="contact-heading">
+          <h1>Contact Form</h1>
         </div>
-        <div className='contact-form-fields'>
-
-        </div>
-        <div className='send-button'>
-
-        </div>
+        <div className="contact-form">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Subject<br />
+              <input type="text" name="subject" />
+            </label>
+            <br />
+            <label>
+              Name<br />
+              <input type="text" name="name" />
+            </label>
+            <br />
+            <label>
+              Email<br />
+              <input type="email" name="email" />
+            </label>
+            <br />
+            <label>
+              Message<br />
+              <textarea rows={4} name="message"></textarea>
+            </label>
+            <br />
+            <div className="submit-button">
+              <button type="submit">Send</button>
+            </div>
+          </form>
         </div>
       </div>
-
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
-// Home.js
+// Home
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 const Home = () => {
@@ -8,12 +9,14 @@ const Home = () => {
       <div className='landing-container'>
         <div className='landing-content'>
           <div className='text-container'>
-            <h1 className='tagline'>Hello.  I'm Matt Pulford, a Full Stack Developer. </h1>
-            <p className='description'> I look forward to collaborating with you, crafting digital experiences that stand out in the vast landscape. Let's embark on a creative journey together.</p>
+            <h1 className='tagline'>Hello. I'm Matt Pulford, a Full Stack Developer. </h1>
+            <p className='description'>I look forward to collaborating with you, crafting digital experiences that stand out in the vast landscape. Let's embark on a creative journey together.</p>
           </div>
           <div className='buttons-container'>
             <button>Portfolio</button>
-            <button className='contact-button'>Contact</button>
+            <Link to='/contact'>
+              <button className='contact-button'>Contact</button>
+            </Link>
           </div>
         </div>
       </div>
