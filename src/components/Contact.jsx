a8c21ad91a2bb701557ba38c12b475f6ae6edf9b
Matt Pulford <mattyp666@outlook.com>
2023-12-31 15:49:32 -0500

    Fix home buttons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,34 +10,26 @@ const Contact = () => {
   return (
     <div className='contact'>
       <div className='contact-area'>
-        <div className="contact-heading">
-          <h1>Contact Form</h1>
-        </div>
         <div className="contact-form">
           <form onSubmit={handleSubmit}>
-            <label>
-              Subject<br />
-              <input type="text" name="subject" />
-            </label>
-            <br />
-            <label>
-              Name<br />
-              <input type="text" name="name" />
-            </label>
-            <br />
-            <label>
-              Email<br />
-              <input type="email" name="email" />
-            </label>
-            <br />
-            <label>
-              Message<br />
-              <textarea rows={4} name="message"></textarea>
-            </label>
-            <br />
+
+            <div className='name-section'>
+              <label for="name">Name</label><br />
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className='email-section'>
+              <label for="email">Email</label><br />
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className='message-section'>
+              <label for="message">Message</label><br />
+              <textarea id="message" name="message" rows="8"></textarea>
+            </div>
             <div className="submit-button">
               <button type="submit">Send</button>
             </div>
+
+
           </form>
         </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,9 @@ const Home = () => {
             <p className='description'>I look forward to collaborating with you, crafting digital experiences that stand out in the vast landscape. Let's embark on a creative journey together.</p>
           </div>
           <div className='buttons-container'>
-            <button>Portfolio</button>
+            <Link to='/recent-projects'>
+              <button>Recent Projects</button>
+            </Link>
             <Link to='/contact'>
               <button className='contact-button'>Contact</button>
             </Link>
